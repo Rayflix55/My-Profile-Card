6095a8bb1f3f462c01698b448f9427a2142fdc65
Rayflix55 <rayflix55@gmail.com>
2025-10-17 14:22:06 -0700

    message

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function updateTime() {
 }
 setInterval(updateTime, 1000);
 updateTime();
+
+// function updateTime() {
+//   const now = new Date();
+//   const hours = now.getHours().toString().padStart(2, '0');
+//   const minutes = now.getMinutes().toString().padStart(2, '0');
+//   const seconds = now.getSeconds().toString().padStart(2, '0');
+//   document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
+// }
+// setInterval(updateTime, 1000);
+// updateTime();
